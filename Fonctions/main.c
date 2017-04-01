b93eb34ac11b0a76697db2d2efca26d3c928df24
Guillaume ROTH <guillaume.roth7@etu.univ-lorraine.fr>
2017-04-01 14:20:11 +0200

    Commit pour FLorian

---
+++ Fonctions/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+double Frequency(int n, int * tab){ // avec tab le tableau de valeur à tester et n sa longueur
+
+}
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
